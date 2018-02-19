3c9f755f5d9d35beef92419e25072e2f19c3a5c9
David Sherman <dsherman@quintor.nl>
2018-02-19 13:55:58 +0100

    Seperate karma config for travis ci

--- karma.travis.conf.js
+++ karma.travis.conf.js
@@ -50,7 +50,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'karma-typescript'],
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
@@ -71,7 +71,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
